abad54c1e60158bfefc0be6ad92f1972d3c0733d
hansoken <62089081+hansoken@users.noreply.github.com>
2020-03-12 15:04:09 +0800

    Add files via upload

--- push.js
+++ push.js
@@ -9,6 +9,11 @@ let vapidKeys = {
 
   push.setVapidDetails('mailto:kenneth@hansoken.com',vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dQOzwMGzo_c:APA91bEWy9cASpM8ExI1P_6A4UKPCk2VyEvMoowzpuEz8uDETJg384ktiRLunOm8NlDj096xYqXDQ-bIoCewaRjpMRtRe-dWSNui007gqIpnB8cD-lgMgmWEhDseH5hL5TDlc-EJMLBD","expirationTime":null,"keys":{"p256dh":"BBEKyBWDJzA060vYedLyaRk7rNJ-3LuE9JsktoGZzU6rzCYfrpFszLl0M2wdFPzQxAeN37Q6XMQiJFpb5h7TgGQ","auth":"xxcCF9I__UHqmfdPAhy6hg"}};
+  let sub = {
+    endpoint:"https://fcm.googleapis.com/fcm/send/dQOzwMGzo_c:APA91bEWy9cASpM8ExI1P_6A4UKPCk2VyEvMoowzpuEz8uDETJg384ktiRLunOm8NlDj096xYqXDQ-bIoCewaRjpMRtRe-dWSNui007gqIpnB8cD-lgMgmWEhDseH5hL5TDlc-EJMLBD",
+    expirationTime:null,
+    keys:{p256dh:
+      "BBEKyBWDJzA060vYedLyaRk7rNJ-3LuE9JsktoGZzU6rzCYfrpFszLl0M2wdFPzQxAeN37Q6XMQiJFpb5h7TgGQ",
+      auth:"xxcCF9I__UHqmfdPAhy6hg"}};
 
   
